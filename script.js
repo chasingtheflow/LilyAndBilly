badbabee6ef60953adfdd65bba2809fab7b617d4
Billy Matthews <Billy.Matthews@bucknell.edu>
2017-09-28 16:06:18 +0200

    Add crest resizing functionality

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-// Activate smooth scrolling on DOMContentLoaded
 $(function() {
+    'use strict';
+
+    // Activate smooth scrolling on DOMContentLoaded
     $('a').smoothScroll({offset: -($('#navbar').height() + 15)});
+
+    // Handle Crest Resizing upon scrolling and window resizing
+    var $c = $('#crest');
+    var initCrestTop = $c.offset().top;
+
+    resizeCrest();
+
+    $(window).resize(handleResize);
+    $(window).scroll(resizeCrest);
+
+    function resizeCrest() {
+        var documentTop = $(document).scrollTop();
+        var crestTop = $c.offset().top;
+        crestTop = crestTop < initCrestTop ? crestTop : initCrestTop;
+
+        if (documentTop > crestTop && !$c.hasClass('shrink')) {
+            $c.addClass('shrink');
+        } else if (documentTop < crestTop && $c.hasClass('shrink')) {
+            $c.removeClass('shrink');
+        }
+    }
+
+    function handleResize() {
+        initCrestTop = $c.offset().top;
+        resizeCrest();
+    }
 });
