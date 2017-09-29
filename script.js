8d3311c7ca231ff0f6457fb77a15400c8af718b6
Billy Matthews <Billy.Matthews@bucknell.edu>
2017-09-29 10:45:20 +0200

    Adjust crest transparency, add placeholder carousel

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function() {
     'use strict';
 
     // Activate smooth scrolling on DOMContentLoaded
-    $('a').smoothScroll({offset: -($('#navbar').height() + 15)});
+    $('a .nav-link').smoothScroll({offset: -($('#navbar').height() + 15)});
 
     // Handle Crest Resizing upon scrolling and window resizing
     var $c = $('#crest');
