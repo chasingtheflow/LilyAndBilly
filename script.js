7092c3f6d8dff76a0560e046769e07d3dec532a7
Billy Matthews <Billy.Matthews@bucknell.edu>
2018-01-22 16:50:44 -0700

    Minor tweaking

--- script.js
+++ script.js
@@ -7,31 +7,31 @@ $(function() {
 
     // Bind the hashchange event listener
     $(window).bind('hashchange', function(event) {
-      $.smoothScroll({
-        // Replace '#/' with '#' to go to the correct target
-        scrollTarget: location.hash.replace(/^\#\/?/, '#'),
-        offset: offset
-      });
+        $.smoothScroll({
+            // Replace '#/' with '#' to go to the correct target
+            scrollTarget: location.hash.replace(/^\#\/?/, '#'),
+            offset: offset
+        });
     });
 
     $('a.nav-link, a[href="#home"]')
-    .bind('click', function(event) {
-      // Remove '#' from the hash.
-      var hash = this.hash.replace(/^#/, '')
+        .bind('click', function(event) {
+            // Remove '#' from the hash.
+            var hash = this.hash.replace(/^#/, '')
 
-      if (this.pathname === location.pathname && hash) {
-        event.preventDefault();
+            if (this.pathname === location.pathname && hash) {
+                event.preventDefault();
 
-        // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
-        location.hash = '#/' + hash;
-      }
-    });
+                // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+                location.hash = '#/' + hash;
+            }
+        });
 
     // Trigger hashchange event on page load if there is a hash in the URL.
     if (location.hash) {
-      $(window).trigger('hashchange');
+        $(window).trigger('hashchange');
     }
-  
+
     // Handle Crest Resizing upon scrolling and window resizing
     var $c = $('#crest');
     var initCrestTop = $c.offset().top;
