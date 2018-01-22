accc0c20328e1c32f182c2b74486abd6a9f06667
Billy Matthews <Billy.Matthews@bucknell.edu>
2018-01-22 15:46:51 -0700

    Adding #home support and minor navigational tweaking

--- script.js
+++ script.js
@@ -2,9 +2,36 @@ $(function() {
     'use strict';
 
     // Activate smooth scrolling on DOMContentLoaded
+    // With Hash based History Support
     var offset = -($('#navbar').height() + 15);
-    $('a.nav-link').smoothScroll({offset: offset});
 
+    // Bind the hashchange event listener
+    $(window).bind('hashchange', function(event) {
+      $.smoothScroll({
+        // Replace '#/' with '#' to go to the correct target
+        scrollTarget: location.hash.replace(/^\#\/?/, '#'),
+        offset: offset
+      });
+    });
+
+    $('a.nav-link, a[href="#home"]')
+    .bind('click', function(event) {
+      // Remove '#' from the hash.
+      var hash = this.hash.replace(/^#/, '')
+
+      if (this.pathname === location.pathname && hash) {
+        event.preventDefault();
+
+        // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+        location.hash = '#/' + hash;
+      }
+    });
+
+    // Trigger hashchange event on page load if there is a hash in the URL.
+    if (location.hash) {
+      $(window).trigger('hashchange');
+    }
+  
     // Handle Crest Resizing upon scrolling and window resizing
     var $c = $('#crest');
     var initCrestTop = $c.offset().top;
